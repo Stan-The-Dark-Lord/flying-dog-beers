be9ff025acdcf9c81d40b8d5b31a4366a8393709
Stan-The-Dark-Lord <66886486+Stan-The-Dark-Lord@users.noreply.github.com>
2020-06-13 13:57:59 -0700

    Update app.py
    Robbys changes

--- app.py
+++ app.py
@@ -1,63 +1,84 @@
+ #-*- coding: utf-8 -*-
+import json
+import base64
+import time
+import random
+import datetime
+from datetime import datetime as dt
+import requests
+import dash_player
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+app = dash.Dash(__name__)
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
+app.scripts.config.serve_locally = True
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
-
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
-
-########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
+def stylemarg (a):
+	return {'margin-bottom': '20px','backgroundColor': a}
+	
+app.layout = html.Div([
+    dash_player.DashPlayer(
+        id='video-player',
+		playing=True,
+        url='/static/ezgifcom-gif-maker_2bmSsLcp_dxTU.mp4',
+        controls=False
     ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
-    ]
-)
+	html.Button(id='color',style={'margin-bottom': '20px','backgroundColor': 'green'} ),
+	html.Div('What is the color?',id='question', style={'margin-bottom': '20px'}),
+    html.Button('green', id='green',n_clicks_timestamp=0),
+    html.Button('blue', id='blue',n_clicks_timestamp=0),
+	html.Button('red', id='red',n_clicks_timestamp=0),
+    html.Div(id='div-current-time', style={'margin-bottom': '20px'}),
+	html.Div(id='timestamp', style={'margin-bottom': '20px'}),
+
+    html.Div(id='div-method-output')
+])
+
+Colo ={
+		1:'red',
+		2:'blue',
+		3:'green'
+		}
+
+
+Data=random.randrange(1, 3)
+
+@app.callback(Output('div-current-time', 'children'),
+              [Input('video-player', 'currentTime')])
+def update_time(currentTime):
+    return 'Current Time: {}'.format(currentTime)
+
+
+@app.callback(Output('div-method-output', 'children'),
+              [Input('video-player', 'secondsLoaded')],
+              [State('video-player', 'duration')])
+def update_methods(secondsLoaded, duration):
+    return 'Second Loaded: {}, Duration: {}'.format(secondsLoaded, duration)
+
+
+
+		
+@app.callback([Output('video-player', 'seekTo'),Output('color', 'style'),Output('timestamp', 'children')],
+              [Input('red', 'n_clicks_timestamp'),Input('blue', 'n_clicks_timestamp'),Input('green', 'n_clicks_timestamp')],
+			  [State('color', 'style')])
+def set_seekTo(red,blue,green,style):
+
+
+
+	if red > green and red > blue and style['backgroundColor']=='red':
+		return None,stylemarg(Colo[random.randrange(1, 3)]), 'tred {} tgreen {} tblue {}'.format(str(red),str(green),str(blue))
+	elif green > red and green > blue and style['backgroundColor']=='green':
+		return None,stylemarg(Colo[random.randrange(1, 3)]),'tred {} tgreen {} tblue {}'.format(str(red),str(green),str(blue))
+	elif blue > green and blue > red and style['backgroundColor']=='blue':
+		return None,stylemarg(Colo[random.randrange(1, 3)]),'tred {} tgreen {} tblue {}'.format(str(red),str(green),str(blue))
+	else:
+		return 0,stylemarg(style['backgroundColor']),'tred {} tgreen {} tblue {}'.format(str(red),str(green),str(blue))
+		
+
+
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
